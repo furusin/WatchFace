e8aa9a8f7c26612c53fc8b8db83ef1d75fc112a4
furusin <furusin.oriver@gmail.com>
2016-05-22 11:48:33 +0900

    HandHeldとWearでListenerのやり取りはできた。

---
+++ mobile/src/main/java/net/furusin/www/watchface/MainActivity.java
@@ -0,0 +1,184 @@
+package net.furusin.www.watchface;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.support.wearable.companion.WatchFaceCompanion;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.Asset;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.PutDataMapRequest;
+import com.google.android.gms.wearable.PutDataRequest;
+import com.google.android.gms.wearable.Wearable;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback<DataApi.DataItemResult> {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private final int READ_REQUEST_CODE = 44;
+    ImageView imageView;
+    GoogleApiClient mGoogleApiClient;
+    private static final String PATH_WITH_FEATURE = "/watch_face_config/Digital";
+    private String mPeerId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        Button button = (Button) findViewById(R.id.button);
+
+        mGoogleApiClient = new GoogleApiClient
+                .Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Wearable.API)
+                .build();
+
+        mPeerId = getIntent().getStringExtra(WatchFaceCompanion.EXTRA_PEER_ID);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/*");
+
+                startActivityForResult(intent, READ_REQUEST_CODE);
+
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                uri = resultData.getData();
+                Log.i("test", "Uri: " + uri.toString());
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    Asset asset = createAssetFromBitmap(bitmap);
+/*
+
+                    PutDataRequest request = PutDataRequest.create("/image");
+                    request.putAsset("profileImage", asset);
+                    Wearable.DataApi.putDataItem(mGoogleApiClient, request);
+
+*/
+                    PutDataMapRequest dataMap = PutDataMapRequest.create("/image");
+                    dataMap.getDataMap().putAsset("profileImage", asset);
+                    PutDataRequest request = dataMap.asPutDataRequest();
+                    PendingResult<DataApi.DataItemResult> pendingResult = Wearable.DataApi.putDataItem(mGoogleApiClient, request);
+                    pendingResult.setResultCallback(new ResultCallback<DataApi.DataItemResult> () {
+                        @Override
+                        public void onResult(DataApi.DataItemResult dataItemResult) {
+                            // something
+                            Log.e("test", "onResult2: " + dataItemResult);
+
+                        }
+                    } );
+
+
+                    imageView.setImageBitmap(bitmap);
+                    Log.i("test", "bitmap set");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static Asset createAssetFromBitmap(Bitmap bitmap) {
+        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteStream);
+        return Asset.createFromBytes(byteStream.toByteArray());
+    }
+
+    @Override
+    public void onStart() {
+        Log.d("test", "onStart");
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d("test", "onStop");
+        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d("test", "onConnected");
+        Uri.Builder builder = new Uri.Builder();
+        Log.d("test", "uri: "+ builder.scheme("wear").path(PATH_WITH_FEATURE).authority(mPeerId).build().toString());
+        Uri uri = builder.scheme("wear").path(PATH_WITH_FEATURE).authority(mPeerId).build();
+        Wearable.DataApi.getDataItem(mGoogleApiClient, uri).setResultCallback(this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d("test", "onConnectionSuspended");
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("test", "onConnectionFailed: " + connectionResult);
+
+    }
+
+    @Override
+    public void onResult(@NonNull DataApi.DataItemResult dataItemResult) {
+        Log.e("test", "onResult: " + dataItemResult);
+
+    }
+}
