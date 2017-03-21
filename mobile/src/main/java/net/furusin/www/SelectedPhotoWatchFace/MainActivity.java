192241691ba9f711b564035890a4dbf55a5063e9
furusin <furusin.oriver@gmail.com>
2017-03-21 15:20:27 +0900

    create viewInterface for MVP model

--- mobile/src/main/java/net/furusin/www/SelectedPhotoWatchFace/MainActivity.java
+++ mobile/src/main/java/net/furusin/www/SelectedPhotoWatchFace/MainActivity.java
@@ -1,21 +1,15 @@
 package net.furusin.www.SelectedPhotoWatchFace;
 
 import android.app.Activity;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.BatteryManager;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.support.wearable.companion.WatchFaceCompanion;
 import android.widget.Toast;
 
@@ -25,18 +19,18 @@ import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.Asset;
 import com.google.android.gms.wearable.DataApi;
-import com.google.android.gms.wearable.DataMap;
 import com.google.android.gms.wearable.PutDataMapRequest;
 import com.google.android.gms.wearable.PutDataRequest;
 import com.google.android.gms.wearable.Wearable;
 
 import net.furusin.www.SelectedPhotoWatchFace.databinding.ActivityMainBinding;
+import net.furusin.www.SelectedPhotoWatchFace.viewInterface.MainViewInterface;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
-        ResultCallback<DataApi.DataItemResult> {
+        ResultCallback<DataApi.DataItemResult>, MainViewInterface {
     private static final String PATH_WITH_FEATURE = "/watch_face_config/Digital";
     private static final int READ_REQUEST_CODE = 44;
 
@@ -46,25 +40,11 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
     private String mPeerId;
 
     @Override
-    protected void onResume() {
-        super.onResume();
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.button);
 
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
@@ -76,33 +56,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         new MyApplication().initAdMob(mBinding.adView);
 
         mPeerId = getIntent().getStringExtra(WatchFaceCompanion.EXTRA_PEER_ID);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                intent.setType("image/*");
-
-                startActivityForResult(intent, READ_REQUEST_CODE);
-            }
-        });
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
 
-        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
-        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
-        // response to some other intent, and the code below shouldn't run at all.
-
         if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
-            // The document selected by the user won't be returned in the intent.
-            // Instead, a URI to that document will be contained in the return intent
-            // provided to this method as a parameter.
-            // Pull that URI using resultData.getData().
             Uri uri = null;
             if (resultData != null) {
                 uri = resultData.getData();
@@ -145,13 +105,9 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
         } else {
             size = bitmap.getWidth();
             croppedBitmap = Bitmap.createBitmap(bitmap, 0, (bitmap.getHeight() / 2) - size / 2, size, size, null, true);
-
         }
-//        bitmap.recycle();
-
         return croppedBitmap;
     }
-
 
     private static Asset createAssetFromBitmap(Bitmap bitmap) {
         final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
@@ -192,5 +148,13 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
     @Override
     public void onResult(@NonNull DataApi.DataItemResult dataItemResult) {
+    }
+
+    @Override
+    public void selectPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+
+        startActivityForResult(intent, READ_REQUEST_CODE);
     }
 }
---
+++ mobile/src/main/java/net/furusin/www/SelectedPhotoWatchFace/viewInterface/MainViewInterface.java
@@ -0,0 +1,9 @@
+package net.furusin.www.SelectedPhotoWatchFace.viewInterface;
+
+/**
+ * Created by furusin on 2017/03/21.
+ */
+
+public interface MainViewInterface {
+    void selectPhoto();
+}
